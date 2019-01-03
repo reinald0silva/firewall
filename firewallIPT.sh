d94aa0ab7bc37e222fdd51a959933076221e0c73
elizot <46333317+elizot@users.noreply.github.com>
2019-01-02 22:42:12 -0300

    Arquivo com modelo de menu simples

--- firewallIPT.sh
+++ firewallIPT.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+while true; do
+	clear	
+	echo -e "\n+------------------------------------+"
+	echo -e "\n|            MENU PRINCIPAL          |"
+	echo -e "\n+------------------------------------+"
+	echo -e "\n|  [1] -                             |"
+	echo -e "\n|  [2] -                             |"
+	echo -e "\n|  [3]                               |"
+	echo -e "\n|  [4] -                             |"
+	echo -e "\n|  [5] -                             |"
+	echo -e "\n|  [6] -                             |"
+	echo -e "\n+------------------------------------+"
+	read -p "ESCOLHA UMA OPÇÃO: " OP
+done
