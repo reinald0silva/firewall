a06a1030a4236b0a6f11ad22dae89ee40b4113cc
elizot <46333317+elizot@users.noreply.github.com>
2019-01-11 13:47:10 -0300

    atualizado

--- firewallIPT.sh
+++ firewallIPT.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
+### BEGIN INIT INFO
+# Provides:          firewall.sh
+# Required-Start:    $local_fs $remote_fs $network $syslog
+# Required-Stop:     $local_fs $remote_fs $network $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start firewall.sh at boot time
+# Description:       Enable service provided by firewall.sh.
+### END INIT INFO
 
-while true; do
-	clear	
-	echo -e "\n+------------------------------------+"
-	echo -e "\n|            MENU PRINCIPAL          |"
-	echo -e "\n+------------------------------------+"
-	echo -e "\n|  [1] -                             |"
-	echo -e "\n|  [2] -                             |"
-	echo -e "\n|  [3]                               |"
-	echo -e "\n|  [4] -                             |"
-	echo -e "\n|  [5] -                             |"
-	echo -e "\n|  [6] -                             |"
-	echo -e "\n+------------------------------------+"
-	read -p "ESCOLHA UMA OPÇÃO: " OP
-done
+IPT=$(which iptables) #/sbin/iptables
+
+function _menu_firewall()
+{
+	clear
+		echo "+-----------------------------------------------+"
+		echo "|               FIREWALL-IPTABLES               |"
+		echo "+-----------------------------------------------+"
+		echo "| [ 1 ] » INICIAR FIREWALL.                     |"
+		echo "| [ 2 ] » DESATIVAR FIREWALL.                   |"
+		echo "| [ 3 ] » LISTAR REGRAS HABILITADAS.            |"
+		echo "| [ 4 ] » REMOVER REGRA HABILITADA.             |"
+		echo "| [ 5 ] » ADICIONAR NOVA REGRA.                 |"
+		echo "| [ 0 ] » FINALIZAR.                            |"
+		echo "+-----------------------------------------------+"
+}
+function _start_fw()
+{
+	
+
+
+
+
+}
