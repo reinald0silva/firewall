a9aa341f76711ac7c290f6b0f088e8a44b815700
elizot <46333317+elizot@users.noreply.github.com>
2019-01-11 18:07:44 -0300

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 ### BEGIN INIT INFO
 # Provides:          firewall.sh
-# Required-Start:    $local_fs $remote_fs $network $syslog
-# Required-Stop:     $local_fs $remote_fs $network $syslog
+# Required-Start:
+# Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Start firewall.sh at boot time
-# Description:       Enable service provided by firewall.sh.
+# Short-Description: Iniciar firewall.sh ao iniciar
+# Description:       executar script firewall.sh ao iniciar.
 ### END INIT INFO
-
+#
+#      Nome do arquivo firewall.sh
+#      Criar arquivo no /etc/firewall/
+#      ln -s /etc/firewall/firewall.sh /etc/init.d
+#      cd /etc/init.d
+#      chkconfig firewall.sh on
+#      Colocando script na inicializacao
+#      Tonar executavel chmod +x /etc/firewall/firewall.sh
+#
 # local dos logs do iptables que por padrão estarão em /var/log/messages ou 
 # em /var/log/kern.log, dependendo da sua distro.
-
-
-
+#
 IPT4=$(which iptables) #/sbin/iptables
 IPT6=$(which ip6tables) #/sbin/iptables
 
@@ -24,21 +30,6 @@ AZUL='\033[1;36m'
 AMAR='\033[1;33m'
 ROSA='\033[1;35m'
 NC='\033[0m'
-
-function _menu_firewall()
-{
-	clear
-		echo "+-----------------------------------------------+"
-		echo "|               FIREWALL-IPTABLES               |"
-		echo "+-----------------------------------------------+"
-		echo "| [ 1 ] » INICIAR FIREWALL PADRÃO.              |"
-		echo "| [ 2 ] » DESATIVAR FIREWALL.                   |"
-		echo "| [ 3 ] » LISTAR REGRAS HABILITADAS.            |"
-		echo "| [ 4 ] » REMOVER REGRA HABILITADA.             |"
-		echo "| [ 5 ] » ADICIONAR NOVA REGRA.                 |" #DROP PORT, ACCEPT PORT, DROP IP, ACCEPT IP, 
-		echo "| [ 0 ] » FINALIZAR.                            |"
-		echo "+-----------------------------------------------+"
-}
 
 function _start()
 {
@@ -139,4 +130,49 @@ echo -e "\t [ ${VERD} Removendo Regras Ipv6 ${NC} ]"
 	$IPT6 -P INPUT ACCEPT
 	$IPT6 -P OUTPUT ACCEPT
 	$IPT6 -P FORWARD ACCEPT
-} 
+}
+
+function _status()
+{
+	echo -e "[${VERDE} Regras IPv4 ${NC}]\n"; echo
+   	$IPT4 -nL
+	echo -e "[${VERDE} Regras IPv6 ${NC}]\n"; echo
+    $IPT6 -nL
+}
+
+function  _add_regras()
+{
+	echo -e "Obs: informe a regra como no ex.: (-A OUTPUT -p tcp --dport xxx -j DROP).\n"
+	select op in add_ipv4 add_ipv6 sair; do
+		case op in
+			1) read -p "REGRA: " regra; $IPT4 ${regra}; echo -e "${VERD} *Regra adicionada ${NC}\n";;
+			2) read -p "REGRA: " regra; $IPT6 ${regra}; echo -e "${VERD} *Regra adicionada ${NC}\n";;
+			3) break ;;
+			*) echo " * Opção inválida!" exit 1
+		esac
+	done
+}
+function _remove_regras()
+{
+	echo -e "Obs: informe a regra como no ex.: (-D OUTPUT -p tcp --dport 22 -j DROP).\n"
+	select op in add_ipv4 add_ipv6 sair; do
+		case op in
+			1) read -p "REGRA IPV4: " regra; $IPT4 ${regra}; echo -e "${VERM} *Regra removida ${NC}\n";;
+			2) read -p "REGRA IPV6: " regra; $IPT6 ${regra}; echo -e "${VERM} *Regra removida ${NC}\n";;
+			3) break ;;
+			*) echo " * Opção inválida!" exit 1
+		esac
+	done
+}
+
+case "$1" in
+	start) echo -e " ${VERD}* Iniciando Firewall ${NC}\n"; _start; echo -e "\nUse: /etc/init.d/firewall status\n"; echo "para verificar as regras" ;;
+	stop)  echo -e "${VERM} * Desativando Firewall ${NC}\n"; _stop;;
+	restart|reload) echo -e "${AMAR} * Reiniciando Firewall ${NC}\n"; _stop;_start;;
+	status) echo -e "${AZUL}Status Firewall ${NC}\n"; _status;;
+	add) echo -e "${ROZA} Adicionar Novas Regras ao Firewall ${NC}\n"; _status;;
+	remove) echo -e "${VERM} Remover Regras do Firewall ${NC}\n"; _remove;;
+	*) echo " * Opção inválida, use: $0 {start|stop|restart|reload|status|add|remove}"; exit 1
+esac
+
+exit 0
