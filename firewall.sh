66f72481f97a322adc57ff095429337fb6579224
elizot <46333317+elizot@users.noreply.github.com>
2019-01-15 00:04:52 -0300

    v2

--- firewall.sh
+++ firewall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Cores vermelho,verde,azul,amarelo,rosa,cor padrão.
+#CORES PARA IMPLEMENTAÇÃO DO CODIGO.
 VM='\033[1;31m' #vermelho
 VD='\033[1;32m' #verde
 AZ='\033[1;36m' #azul
@@ -9,7 +9,7 @@ RS='\033[1;35m' #rosa
 NC='\033[0m' #neutro
 PS='\033[5;30m' # piscar
 #|
-
+#BUSCA POR EXECUTÁVEIS COMO O COMANDO WICH NOS PATHs EXPORTADOS.
 IPT4=$(which iptables) #/sbin/iptables
 IPT6=$(which ip6tables) #/sbin/ip6tables
 IPTS4=$(which iptables-save) #/sbin/iptables-save
@@ -17,24 +17,29 @@ IPTR4=$(which iptables-restore) #/sbin/iptables-restore
 IPTS6=$(which ip6tables-save) #/sbin/ip6tables-save
 IPTR6=$(which ip6tables-restore) #/sbin/ip6tables-restore
 
+#FUNÇÃO DO CABEÇALHO DO SCRIPT.
 function _top()
 {
 	clear
 	echo -e "\t${VD}    FIREWALL ${NC}"
 	echo -e "\t${VM}┬┴┬┴┤${NC}${AZ}(◣_◢)${NC}${VM}├┬┴┬┴${NC}\n"
 }
+
+#function _stop_firewall()
+#{}
+
+#FUNÇÃO ONDE MOSTRA AS REGRAS JÁ ABILITADAS NO FIREWALL-IPTABLES.
 function _status()
 {
 	_top
 	echo -e "\t[${PS}${VD}Regras IPv4 ${NC}]\n"
-   	${IPT4} -nL
+   	${IPT4} -nL --line-numbers
 	echo -e "\n\t[${PS}${VD}Regras IPv6 ${NC}]\n"
-    ${IPT6} -nL
+    ${IPT6} -nL --line-numbers
     echo
     read -p "Precione Enter Para Contiunar..." x
 }
-
-
+#FUNÇÃO PARA FAZER O BACKUP DAS REGRAS ANTERIOS ANTES DAS MUDANÇAS, PARA CASO DE ERROS.
 function _salvar_regras_atu()
 {
 	_top
@@ -66,11 +71,10 @@ function _salvar_regras_atu()
 			esac
 		done
 	fi
-	
-	
 	read -p "Precione Enter Para Contiunar..." x
 }
 
+#RESTAURA O BACKUP DAS REGRAS SALVAR PELA FUNÇÃO ANTERIOR.
 function _restaurar_regras_sv()
 {
 	_top
@@ -90,7 +94,6 @@ function _restaurar_regras_sv()
 			esac
 		done
 	fi
-	
 	if [ $(which ip6tables-restore) ]; then
 		echo -e "${AZ}ip6tables-restore${NC} [${PS}${VD}OK${NC}]"
 		$IPTR6 < /etc/ip6tables-save; echo -e "${VERM} *Regras ipv6 atuais restauradas ${NC}\n"
@@ -111,6 +114,7 @@ function _restaurar_regras_sv()
 	read -p "Precione Enter Para Contiunar..." x
 }
 
+#FUNÇÃO PARA EXECUTAR A REMOÇÃO DAS REGRAS PELO INDICE DE CADA REGRAS DA CADEIA INPUT.
 function _INPUT_RM()
 {
 	_top
@@ -125,7 +129,7 @@ function _INPUT_RM()
 	fi	
 	read -p "Precione Enter Para Contiunar..." x
 }
-
+#FUNÇÃO PARA EXECUTAR A REMOÇÃO DAS REGRAS PELO INDICE DE CADA REGRAS DA CADEIA FORWARD.
 function _FORWARD_RM()
 {
 	_top
@@ -140,7 +144,7 @@ function _FORWARD_RM()
 	fi	
 	read -p "Precione Enter Para Contiunar..." x
 }
-
+#FUNÇÃO PARA EXECUTAR A REMOÇÃO DAS REGRAS PELO INDICE DE CADA REGRAS DA CADEIA OUTPUT.
 function _OUTPUT_RM()
 {
 	_top
@@ -156,31 +160,31 @@ function _OUTPUT_RM()
 	read -p "Precione Enter Para Contiunar..." x
 }
 
-
+#FUNÇÃO COM MENU DE SELEÇÃO PARA ESCOLHER QUEL CADEIA IRA INICIAR PARA FAZER A REMOÇÃO DAS REGRAS.
 function _remove_regras_cadeias()
 {
 	_top
 	PS3="Selecione uma cadeia acima: "
-	select OP in INPUT FORWARD OUTPUT; do
+	select OP in INPUT FORWARD OUTPUT sair; do
 		case ${REPLY} in 
 			1) _INPUT_RM; break ;;
 			2) _FORWARD_RM; break ;;
 			3) _OUTPUT_RM; break ;;
 			4) break ;;
-			*) echo "${VM}Opção Invalida!${NC}"
+			*) echo -e "${VM}Opção Invalida!${NC}"
 		esac
 	done
 }
-
+#FUNÇÃO COM MENU PRINCIPAL DE SELEÇÃO PARA ESCOLHA.
 for ((i=1;i>0;i++)); do
 	_top
 	echo -e "${VM}[1]${NC} - ${AM}Iniciar Firewall.${NC}"
 	echo -e "${VM}[2]${NC} - ${AM}Parar Firewall.${NC} "
-	echo -e "${VM}[3]${NC} - ${AM}Status Firewall.${NC}"
-	echo -e "${VM}[4]${NC} - ${AM}Salvar Regras Atuais.${NC}"
-	echo -e "${VM}[5]${NC} - ${AM}Restaurar Regras Salvas.${NC}"
+	echo -e "${VM}[3]${NC} - ${AM}Status Firewall.${NC}" #FEITO
+	echo -e "${VM}[4]${NC} - ${AM}Salvar Regras Atuais.${NC}" #FEITO
+	echo -e "${VM}[5]${NC} - ${AM}Restaurar Regras Salvas.${NC}" #FEITO
 	echo -e "${VM}[6]${NC} - ${AM}Adicionar Regras.${NC}"
-	echo -e "${VM}[7]${NC} - ${AM}Remover Regras.${NC}"
+	echo -e "${VM}[7]${NC} - ${AM}Remover Regras.${NC}" #FEITO
 	echo -e "${VM}[0]${NC} - ${VM}Sair.${NC}"
 	read -p "Selecione uma opção: " OP
 		case $OP in 
